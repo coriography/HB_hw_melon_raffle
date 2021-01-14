dc149939dd8a76f38902bb279529a24421cf5e68
Cori Lint <corilint@gmail.com>
2021-01-14 11:58:36 -0600

    Added functions to raffle.py and imported modules

--- version2/raffle.py
+++ version2/raffle.py
@@ -1,6 +1,24 @@
 """Randomly pick customer and print customer info"""
 
-# Add code starting here
+import random
+from customers import get_customers_from_file
 
-# Hint: remember to import any functions you need from
-# other files or libraries
+# import functions from customers.py
+# add functions below
+
+def pick_winner(customers):
+    """Choose a random winner from list of customers."""
+
+    chosen_customer = random.choice(customers)
+
+    print("Tell {name} at {email} that they've won".format(
+        name=chosen_customer.name,
+        email=chosen_customer.email
+        ))
+
+
+def run_raffle():
+    """Run the weekly raffle."""
+
+    customers = get_customers_from_file("customers.txt")
+    pick_winner(customers)
